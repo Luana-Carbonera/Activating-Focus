60dc470c37e035371a238a3db7632f6294c71481
Luana Carbonera <luana.lcarbonera@gmail.com>
2022-09-23 20:55:19 -0300

    Estudando Javascript

---
+++ Javascript/7-Condicionais.js
@@ -0,0 +1,17 @@
+console.log(`Trabalhando com condicionais`);
+
+
+const idadeComprador = 12;
+const estaAcompanhada = true
+
+if (idadeComprador >= 18) {
+    console.log("comprador maior de idade");
+} else if (estaAcompanhada) {
+    console.log("comprador está acompanhado");
+} else {
+    console.log("comprador não é maior de idade e não posso vender");
+}
+
+// || - significa ou 
+// && - significa e
+
