3c1d3c9f2c4ae5e4c604330c85f369db6d39c19f
Luana Carbonera <luana.lcarbonera@gmail.com>
2022-10-05 10:22:33 -0300

    Aprendendo Javascript

--- Introdução Javascript/4-ConversaoDeTipos.js
+++ Introdução Javascript/4-ConversaoDeTipos.js
@@ -6,3 +6,16 @@ console.log(parseInt("2") + parseInt("2"));
 
 console.log("10" / "2"); //obriga a converter em numero
 console.log("Ricardo" / "2"); //NaN - Not a Number
+
+// conversão implícita
+
+const numero = 456;
+const numeroString = "456";
+console.log(numero === numeroString);
+console.log(numero == numeroString);
+
+console.log(numero + numeroString);
+
+//conversão explícita
+
+console.log(numero + Number(numeroString));
--- Introdução Javascript/5-AtribuicaoDeVariaveis.js
+++ Introdução Javascript/5-AtribuicaoDeVariaveis.js
@@ -10,3 +10,31 @@ console.log(nome + " " + sobrenome);
 console.log(`Meu nome é ${nome}`);
 
 nome = nome + sobrenome;
+
+// var
+//var altura  = 5;
+//var comprimento = 7;
+//var area = altura * comprimento;
+//console.log(area);
+
+// let
+
+let forma = "retangulo";
+let alturaR = 2;
+let comprimentoR = 7;
+let areaR;
+
+if (forma === "retângulo") {
+    areaR = alturaR * comprimentoR;
+}   else {
+    areaR = (alturaR * comprimentoR) / 2;
+}
+console.log(areaR);
+
+//const
+
+const formaQ = "quadrado";
+const ladoQ = 4;
+const areaQ = ladoQ ** 2;
+console.log(areaQ);
+
