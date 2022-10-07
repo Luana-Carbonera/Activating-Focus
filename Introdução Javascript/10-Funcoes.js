66d2f305c3b6c0317e27db8819947dd06e8452ac
Luana Carbonera <luana.lcarbonera@gmail.com>
2022-10-07 16:37:40 -0300

    Aprendendo Javascript

---
+++ Introdução Javascript/10-Funcoes.js
@@ -0,0 +1,16 @@
+// 1 - declarar a função
+function imprimeTexto(texto) {
+    console.log(texto)
+}
+
+// 2- executar a função
+imprimeTexto("oi");
+imprimeTexto("tchau");
+imprimeTexto("oi");
+
+// há três formas de escrever funções
+// return tem que ser a ultima tarefa do bloco
+function soma() {
+    return 2+2;
+}
+console.log(soma());
