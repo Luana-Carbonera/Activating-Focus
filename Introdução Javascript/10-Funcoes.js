0570fd1f97f2f2acd05c0bd9d1d36b21da353a5b
Luana Carbonera <luana.lcarbonera@gmail.com>
2022-10-08 20:16:09 -0300

    Aprendendo Javascript

--- Introdução Javascript/10-Funcoes.js
+++ Introdução Javascript/10-Funcoes.js
@@ -14,3 +14,23 @@ function soma() {
     return 2+2;
 }
 console.log(soma());
+
+// parâmetros
+
+ function subtracao(num1, num2) {
+    return num1 - num2;
+ }
+ console.log(subtracao(4, 2));
+ console.log(subtracao(100, 100));
+
+ // ordem dos parâmetros
+
+ function nomeIdade(nome, idade) {
+    return `meu nome é ${nome} e minha idade é ${idade}`;
+ }
+ console.log(nomeIdade(40, "Luana"));
+
+ function multiplicacao(numero1, numero2) {
+    return numero1 * numero2;
+ }
+ console.log(multiplicacao(subtracao(12, 2), subtracao(4,1)));
