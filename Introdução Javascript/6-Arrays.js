3ec411a9bd651b19923e48de3802c89589d047fc
Luana Carbonera <luana.lcarbonera@gmail.com>
2022-10-09 15:56:17 -0300

    Aprendendo Javascript

--- Introdução Javascript/6-Arrays.js
+++ Introdução Javascript/6-Arrays.js
@@ -50,3 +50,17 @@ jantar.push('🥗')
 jantar.push('🍏')
 
 console.log(jantar)
+
+// slice()
+
+const nomes = [
+    "joão", "juliana", "Ana", "Caio", "Lara", "Marjorie", "Guilherme",
+     "Aline", "Fabiana", "Andre", "Carlos", "Paulo", "Bia", "Vivian", 
+    "Isabela", "Vinicius", "Renan", "Renata", "Daisy", "Camilo"
+    ];
+
+const sala1 = nomes.slice(0, nomes.length/2);
+const sala2 = nomes.slice(nomes.length/2);
+
+console.log(sala1);
+console.log(sala2);
