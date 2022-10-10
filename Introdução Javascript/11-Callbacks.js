1310ac268693258811868f25e1a823ebcde7e50d
Luana Carbonera <luana.lcarbonera@gmail.com>
2022-10-10 14:34:40 -0300

    Aprendendo Javascript

--- Introdução Javascript/11-Callbacks.js
+++ Introdução Javascript/11-Callbacks.js
@@ -6,7 +6,7 @@ function ImprimeNomes(nome){
     console.log(nome)
 }
 
-const notas = [10, 9, 8, 7, 6];
+const notas = [7, 4.5, 8, 7.6];
 const notasAtualizadas = notas.map( nota => nota == 10 ? nota : ++nota);
 console.log(notasAtualizadas);
 
@@ -20,3 +20,38 @@ const nomeDaPlataforma = " Alura"
 
 const nomeCompleto = nomedoCurso.concat(nomeDaPlataforma)
 console.log(nomeCompleto)  //Fundamentos de JS Alura
+
+
+const reprovados = nome.filter( (aluno, indice) => notas[indice] < 5);
+console.log(`reprovados; ${reprovados}`);
+
+
+const salaJS = [7, 8, 8, 7, 10, 6.5, 4, 10, 7];
+const salaJava = [6, 5, 8, 9, 5, 6];
+const salaPython = [7, 3.5, 8, 9.5];
+
+function mediaSala(notasDasSalas) {
+    const somaSalas = notasDasSalas.reduce((acum, atual) => 
+    atual + acum, 0)
+    return somaSalas/notasDasSalas.length
+}
+
+console.log(mediaSala(salaJS))
+console.log(mediaSala(salaJava))
+console.log(mediaSala(salaPython))
+
+const media = notas.reduce((acum, atual) => atual + acum, 0) / notas.length;
+console.log(media);
+
+
+const numeros = [43, 50, 65, 12]
+
+const soma = numeros.reduce((acum, atual) => atual + acum, 0)
+
+console.log(soma) //170
+
+function operacaoNumerica(acum, atual) {
+    return atual + acum
+   }
+   
+   const soma2 = numeros.reduce(operacaoNumerica, 0)
