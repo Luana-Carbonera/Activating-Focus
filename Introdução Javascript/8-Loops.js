b6d1bde9eb975d808f7afd7f27d2c7198a237173
Luana Carbonera <luana.lcarbonera@gmail.com>
2022-10-10 12:39:01 -0300

    Aprendendo Javascript

--- Introdução Javascript/8-Loops.js
+++ Introdução Javascript/8-Loops.js
@@ -45,3 +45,19 @@ const numeros = [100, 200, 300, 400, 500, 600];
 for (let i = 0; i < numeros.length; i++) {
     console.log(i, numeros[i])
 }
+
+const notas = [10, 6.5, 8, 7.5];
+
+let somaDasNotas = 0
+
+//for(let i = 0; i < notas.length; i++) {
+//    somaDasNotas += notas[i];
+//}
+
+//console.log(media)
+
+notas.forEach(nota =>{
+    somaDasNotas += nota
+})
+let media = somaDasNotas/notas.length;
+console.log(media);
